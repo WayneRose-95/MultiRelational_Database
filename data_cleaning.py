92c930b1c844adb1936f5ea222f046e1c81e332f
Wayne Rose <e.guishardrose@hotmail.co.uk>
2023-05-24 22:08:05 +0100

    creating method to perform ETL process for cleaning the store_table

--- data_cleaning.py
+++ data_cleaning.py
@@ -80,15 +80,87 @@ class DataCleaning:
         legacy_users_dataframe['user_key'] = legacy_users_dataframe.index 
         upload = DatabaseConnector() 
         try:
-            upload.upload_to_db(legacy_users_dataframe,self.engine, 'dim_users')
+            upload.upload_to_db(legacy_users_dataframe, self.engine, 'dim_users')
             print(f"Table uploaded")
         except: 
             print("there was an error")
             raise Exception 
         return legacy_users_dataframe
     
+    def clean_store_data(self):
+        # Instantiating an instance of the Database Extractor class 
+        extractor = DatabaseExtractor()
+        # Reading in the table from the AWS database 
+        legacy_store_dataframe = extractor.read_rds_table("legacy_store_details")
+        
+        # State the column names for the table 
+        legacy_store_dataframe.columns = [
+            'store_key',
+            'store_address', 
+            'longitude', 
+            'null_column', 
+            'city', 
+            'store_code', 
+            'store_number', 
+            'opening_date', 
+            'store_type', 
+            'latitude',
+            'country_code', 
+            'region'
+        
+        ]
+        
+        # Drop the null_column column within the table 
+        legacy_store_dataframe = legacy_store_dataframe.drop('null_column', axis=1)
+
+        # Reorder the column order 
+        column_order = [
+            'store_key',
+            'store_address',
+            'longitude',
+            'latitude',
+            'city',
+            'store_code',
+            'store_number',
+            'opening_date',
+            'store_type',
+            'country_code',
+            'region'
+        ]
+
+        # remake the dataframe with the column order in place 
+        legacy_store_dataframe = legacy_store_dataframe[column_order]
+
+        # Change the date format of the opening_date column to dd/mm/yyyy 
+        legacy_store_dataframe["opening_date"] = pd.to_datetime(legacy_store_dataframe['opening_date'], errors='coerce').dt.strftime('%d/%m/%Y')
+
+        # Drop dates in the opening_date which are null 
+        legacy_store_dataframe = legacy_store_dataframe.dropna(subset=['opening_date'])
+
+        # Reset the index if desired
+        legacy_store_dataframe = legacy_store_dataframe.reset_index(drop=True)
+        
+        # Set the store_key column as the index column to reallign it with the index column 
+        legacy_store_dataframe['store_key'] = legacy_store_dataframe.index 
+
+        # Lastly, drop the store_number column
+        legacy_store_dataframe = legacy_store_dataframe.drop("store_number", axis=1)
+
+        legacy_store_dataframe.replace('eeEurope', 'Europe')
+        legacy_store_dataframe.replace('eeAmerica', 'America')
+
+        upload = DatabaseConnector() 
+        try:
+            upload.upload_to_db(legacy_store_dataframe, self.engine, 'dim_store_details')
+            print(f"Table uploaded")
+        except: 
+            print("there was an error")
+            raise Exception 
+        return legacy_store_dataframe
+        
 
 if __name__=="__main__":
     cleaner = DataCleaning()
-    cleaner.clean_user_data()
+    # cleaner.clean_user_data()
+    cleaner.clean_store_data() 
  
