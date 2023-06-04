619098cdf371bbedcecdd596e5b30f00f9f34f46
Wayne Rose <e.guishardrose@hotmail.co.uk>
2023-06-04 21:48:01 +0100

    Fixed SQL scripts to map the foreign keys to the correct information in the dimension tables. Updating next steps document to involve queyring the data.

--- sales_data/DML/update_orders_table_foreign_keys.sql
+++ sales_data/DML/update_orders_table_foreign_keys.sql
@@ -1,4 +1,5 @@
 -- 04.06.2023 WR Initial Draft 
+-- 04.06.2023 WR 21:41PM GMT Updated script to match keys to the orders_table
 
 -- Updating orders_table with the keys from the dim_card_details table 
 UPDATE orders_table
@@ -10,22 +11,22 @@ WHERE orders_table.card_number::varchar = dim_card_details.card_number;
 UPDATE orders_table
 SET date_key = dim_date_times.time_key
 FROM dim_date_times
-WHERE orders_table.date_key = dim_date_times.time_key;
+WHERE orders_table.date_uuid::uuid = dim_date_times.date_uuid;
 
 -- Updating orders_table with the keys from the dim_product_details table
 UPDATE orders_table 
 SET product_key = dim_product_details.product_key
 FROM dim_product_details 
-WHERE orders_table.product_key = dim_product_details.product_key;
+WHERE orders_table.product_code::varchar = dim_product_details.product_code;
 
 -- Updating orders_table with the keys from the dim_store_details table
 UPDATE orders_table 
 SET store_key = dim_store_details.store_key 
 FROM dim_store_details
-WHERE orders_table.store_key = dim_store_details.store_key;
+WHERE orders_table.store_code::varchar = dim_store_details.store_code;
 
 -- Updating orders_table with keys from the dim_users table 
 UPDATE orders_table 
 SET user_key = dim_users.user_key 
 FROM dim_users 
-WHERE orders_table.user_key = dim_users.user_key; 
+WHERE orders_table.user_uuid::uuid = dim_users.unique_id; 
