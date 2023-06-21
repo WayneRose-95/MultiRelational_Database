d37a8d10a2948499ecff02b702619dbc8530a8f8
Wayne Rose <e.guishardrose@hotmail.co.uk>
2023-06-21 18:07:11 +0100

    modified database_utils.py script to fit with unittest script

--- database_utils.py
+++ database_utils.py
@@ -2,9 +2,10 @@ import yaml
 from sqlalchemy import create_engine
 from sqlalchemy.exc import OperationalError 
 import pandas as pd 
+import os 
 
 class DatabaseConnector:
-    def read_database_credentials(self, config_file):
+    def read_database_credentials(self, config_file: yaml):
         '''
         Method to read database_credentials from a yaml file 
 
@@ -17,21 +18,48 @@ class DatabaseConnector:
         A dictionary of the database credentials from yaml file 
 
         '''
+
+        # Check if the file extension is YAML
+        valid_extensions = ['.yaml', '.yml']
+        name_of_file, file_extension = os.path.splitext(config_file)
+        if file_extension.lower() not in valid_extensions:
+            raise ValueError("Invalid file extension. Only YAML files are allowed.")
         # Read the yaml file
         try:
             with open(config_file) as file:
                 database_credentials = yaml.safe_load(file)
-                # print(database_credentials)
-
+                
              # Return the yaml file as a dictionary    
             return database_credentials
         # If the file is not found, raise an exception 
         except FileNotFoundError:
-            raise Exception("Config file not found")
+            raise FileNotFoundError("Config file not found")
         # If the config file is not in a YAML format, raise an exception
         except yaml.YAMLError:
-            raise Exception("Invalid YAML format.")
+            raise yaml.YAMLError("Invalid YAML format.")
+
+    def create_connection_string(self, config_file_name):
+        '''
+        Method to create the connection_string needed to connect to a postgresql database 
+
+        Parameters: 
+        config_file_name: 
+        The pathway to the config_file used to create the string 
+
+        Returns: 
+        Connection string : str 
+        A string used to connect to the database 
+
+        '''
+        # Call the database details method to use the dictionary as an output
+        database_credentials = self.read_database_credentials(config_file_name)
+        # If the database_credentials method fails 
+        # i.e. recieves the wrong weapon type 
+        if not database_credentials:
+            raise Exception("Invalid database credentials.")
         
+        connection_string = f"postgresql+psycopg2://{database_credentials['RDS_USER']}:{database_credentials['RDS_PASSWORD']}@{database_credentials['RDS_HOST']}:{database_credentials['RDS_PORT']}/{database_credentials['RDS_DATABASE']}"
+        return connection_string
 
     def initialise_database_connection(self, config_file_name):
         '''
@@ -46,17 +74,8 @@ class DatabaseConnector:
         A database engine object 
 
         '''
-        # Call the database details method to use the dictionary as an output
-        database_credentials = self.read_database_credentials(config_file_name)
-
-        # Initialise an empty list representing the connection string needed for create engine
-
-        if not database_credentials:
-            raise Exception("Invalid database credentials.")
         
-
-        connection_string = f"postgresql+psycopg2://{database_credentials['RDS_USER']}:{database_credentials['RDS_PASSWORD']}@{database_credentials['RDS_HOST']}:{database_credentials['RDS_PORT']}/{database_credentials['RDS_DATABASE']}"
-        
+        connection_string = self.create_connection_string(config_file_name)
 
         # Lastly try to connect to the database using your connection string variable
         try:
@@ -66,7 +85,7 @@ class DatabaseConnector:
             return database_engine
         except OperationalError:
             print("Error Connecting to the Database")
-            raise Exception
+            raise OperationalError 
     
         
          
@@ -94,5 +113,5 @@ class DatabaseConnector:
 
 if __name__ == "__main__":
     new_database = DatabaseConnector()
-    # new_database.read_database_credentials('db_creds.yaml') 
-    new_database.initialise_database_connection('db_creds.yaml')
+    new_database.read_database_credentials('test_yaml.yaml') 
+    # new_database.initialise_database_connection('db_creds.yaml')
