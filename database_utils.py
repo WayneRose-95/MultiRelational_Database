9cb1fbe4f60f9373fe086c51fdd5c284c37e6d50
Wayne Rose <e.guishardrose@hotmail.co.uk>
2023-06-23 15:17:42 +0100

    Initial draft for logger.py script and applied modifications to database_utils.py script

--- database_utils.py
+++ database_utils.py
@@ -1,8 +1,12 @@
 import yaml
 from sqlalchemy import create_engine
 from sqlalchemy.exc import OperationalError 
+from logger import DatabaseLogger
 import pandas as pd 
 import os 
+
+
+logger = DatabaseLogger("logs/database_utils.log")
 
 class DatabaseConnector:
     def read_database_credentials(self, config_file: yaml):
@@ -20,22 +24,33 @@ class DatabaseConnector:
         '''
 
         # Check if the file extension is YAML
+        logger.info(f"Reading {config_file}")
         valid_extensions = ['.yaml', '.yml']
         name_of_file, file_extension = os.path.splitext(config_file)
+        logger.info("Validating file extension")
+        logger.info(name_of_file)
+        logger.info(file_extension)
         if file_extension.lower() not in valid_extensions:
+            logger.exception("Invalid file extension. Only YAML files are allowed")
             raise ValueError("Invalid file extension. Only YAML files are allowed.")
         # Read the yaml file
+        logger.info(f"{config_file} successfully validated")
         try:
+            logger.info(f"Opening {config_file}")
             with open(config_file) as file:
                 database_credentials = yaml.safe_load(file)
-                
+                logger.info(f"Config file {config_file} loaded")
+                logger.info(database_credentials)
              # Return the yaml file as a dictionary    
             return database_credentials
         # If the file is not found, raise an exception 
         except FileNotFoundError:
+            logger.exception(f"Logging file {config_file} not found" )
             raise FileNotFoundError("Config file not found")
         # If the config file is not in a YAML format, raise an exception
         except yaml.YAMLError:
+            logger.exception(f"Invalid file format" )
+            logger.critical("Invalid .yaml file. Please check the formatting of your .yaml / .yml file")
             raise yaml.YAMLError("Invalid YAML format.")
 
     def create_connection_string(self, config_file_name):
@@ -59,6 +74,7 @@ class DatabaseConnector:
             raise Exception("Invalid database credentials.")
         
         connection_string = f"postgresql+psycopg2://{database_credentials['RDS_USER']}:{database_credentials['RDS_PASSWORD']}@{database_credentials['RDS_HOST']}:{database_credentials['RDS_PORT']}/{database_credentials['RDS_DATABASE']}"
+        logger.debug(f"Connection string created {connection_string}")
         return connection_string
 
     def initialise_database_connection(self, config_file_name):
@@ -79,11 +95,15 @@ class DatabaseConnector:
 
         # Lastly try to connect to the database using your connection string variable
         try:
+            logger.info("Creating database engine using sqlaclhemy's create_engine")
             database_engine = create_engine(connection_string)
+            logger.info(f"Database Engine : {database_engine} created attempting to connect")
             database_engine.connect()
+            logger.info("connection successful")
             print("connection successful")
             return database_engine
         except OperationalError:
+            logger.exception("OperationalError detected. Error connecting to the database")
             print("Error Connecting to the Database")
             raise OperationalError 
     
@@ -104,8 +124,11 @@ class DatabaseConnector:
         The name of the table to be uploaded to the database 
         '''
         try:
+            logger.info(f"Attempting to upload table {table_name} to the database")
+            
             dataframe.to_sql(table_name, con=connection, if_exists='replace')
         except:
+            logger.exception(f"Error uploading table to the database. Connection used : {connection}")
             print("Error uploading table to the database")
             raise Exception 
         pass
@@ -113,5 +136,6 @@ class DatabaseConnector:
 
 if __name__ == "__main__":
     new_database = DatabaseConnector()
-    new_database.read_database_credentials('test_yaml.yaml') 
+    new_database.read_database_credentials('test_config_yaml.yaml') 
+    # new_database.create_connection_string('sales_data_creds_dev.yaml')
     # new_database.initialise_database_connection('db_creds.yaml')
---
+++ logger.py
@@ -0,0 +1,34 @@
+import os
+import logging
+
+class DatabaseLogger:
+    def __init__(self, log_filename):
+        if not os.path.exists(log_filename):
+            os.makedirs(os.path.dirname(log_filename), exist_ok=True)
+
+        self.logger = logging.getLogger(__name__)
+        self.logger.setLevel(logging.DEBUG)
+
+        # Format the logs by time, filename, function_name, level_name, and the message
+        formatter = logging.Formatter("%(asctime)s:%(filename)s:%(funcName)s:%(levelname)s:%(message)s")
+        file_handler = logging.FileHandler(log_filename)
+        file_handler.setFormatter(formatter)
+        self.logger.addHandler(file_handler)
+
+    def info(self, message):
+        self.logger.info(message)
+
+    def exception(self, message):
+        self.logger.exception(message)
+
+    def critical(self, message):
+        self.logger.critical(message)
+
+    def debug(self, message):
+        self.logger.debug(message)
+
+    def error(self, message):
+        self.logger.error(message)
+
+
+
