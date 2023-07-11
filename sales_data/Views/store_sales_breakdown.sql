ded4848a0f4d1ff88d7ca061ca5ac66804b73b00
Wayne Rose <e.guishardrose@hotmail.co.uk>
2023-07-11 20:45:24 +0100

    modifying views to accomodate new changes to column names

--- sales_data/Views/store_sales_breakdown.sql
+++ sales_data/Views/store_sales_breakdown.sql
@@ -16,7 +16,7 @@ CREATE OR REPLACE VIEW public.store_sales_breakdown
    FROM dim_store_details ds
      JOIN orders_table o ON ds.store_key = o.store_key
      JOIN dim_product_details dp ON o.product_key = dp.product_key
-     JOIN dim_date_times dt ON o.date_key = dt.time_key
+     JOIN dim_date_times dt ON o.date_key = dt.date_key
   GROUP BY ds.store_type, dp.product_price, o.product_quantity, ds.city, ds.region, dt.day, dt.month, dt.year, dt.time_period
   ORDER BY (round((sum(dp.product_price * o.product_quantity::double precision) / sum(sum(dp.product_price * o.product_quantity::double precision)) OVER ())::numeric * 100::numeric, 2)) DESC;
 
